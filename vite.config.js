7738c506a6b61ba5c5739606d853a5ad569a2d27
rulik04 <rulik_2004@mail.ru>
2025-06-25 16:11:34 +0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://vue-jobs-xm0d.onrender.com/',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
