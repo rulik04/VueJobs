67bb4ce0f7e54ab8d71bee9ef50c1fb016ad2c7b
rulik04 <rulik_2004@mail.ru>
2025-06-25 16:20:33 +0500

    fix add job

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -34,7 +34,7 @@ const handleSubmit = async () => {
     }
 
     try {
-        const response = await axios.post(`/api/jobs/`, newJob)
+        const response = await axios.post(`/api/jobs`, newJob)
         toast.success('Job Added Successfully')
         router.push(`/jobs/${response.data.id}`)
     } catch (error) {
